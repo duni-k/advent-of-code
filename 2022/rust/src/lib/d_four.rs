40e9345b2279372c413a07bf085822dd06118e9c
Edvard All <edvard.all@gmail.com>
2022-12-04 15:26:36 +0100

    no hashset

--- 2022/rust/src/lib/d_four.rs
+++ 2022/rust/src/lib/d_four.rs
@@ -1,37 +1,40 @@
-use std::{collections::HashSet, ops::RangeInclusive};
-
 pub fn count_supersets(lines: impl Iterator<Item = String>) -> usize {
-    let anyone_contains =
-        |h1: &HashSet<_>, h2: &HashSet<_>| -> bool { h1.is_superset(h2) || h2.is_superset(h1) };
-    count_with_func(lines, anyone_contains)
+    count_with_func(lines, |(a, b), (c, d)| {
+        (a >= c && b <= d) || (c >= a && d <= b)
+    })
 }
 
 pub fn count_intersections(lines: impl Iterator<Item = String>) -> usize {
-    let has_intersection =
-        |h1: &HashSet<_>, h2: &HashSet<_>| -> bool { h1.intersection(h2).count() > 0 };
     count_with_func(lines, has_intersection)
+}
+
+fn has_intersection((lo1, hi1): &(usize, usize), (lo2, hi2): &(usize, usize)) -> bool {
+    (lo1 >= lo2 && lo1 <= hi2)
+        || (hi1 >= lo2 && hi1 <= hi2)
+        || (lo2 >= lo1 && lo2 <= hi1)
+        || (hi2 >= lo1 && hi2 <= hi1)
 }
 
 fn count_with_func<F>(lines: impl Iterator<Item = String>, f: F) -> usize
 where
-    F: Fn(&HashSet<usize>, &HashSet<usize>) -> bool,
+    F: Fn(&(usize, usize), &(usize, usize)) -> bool,
 {
-    let mut count = 0;
-    for line in lines {
-        let (sec1, sec2) = line.split_once(',').expect("Failed splitting pair.");
-        let (r1, r2) = (parse_range(sec1), parse_range(sec2));
-        let (h1, h2): (HashSet<usize>, HashSet<usize>) =
-            (HashSet::from_iter(r1), HashSet::from_iter(r2));
-        if f(&h1, &h2) {
-            count += 1;
-        }
-    }
-    count
+    lines
+        .map(|line| {
+            let (sec1, sec2) = line.split_once(',').expect("Failed splitting pair.");
+            let (r1, r2) = (parse_range(sec1), parse_range(sec2));
+            if f(&r1, &r2) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
-fn parse_range(range: &str) -> RangeInclusive<usize> {
+fn parse_range(range: &str) -> (usize, usize) {
     let (lo, hi) = range.split_once('-').expect("Failed splitting range.");
     let lo = lo.parse::<usize>().expect("Failed parsing as number.");
     let hi = hi.parse::<usize>().expect("Failed parsing as number.");
-    lo..=hi
+    (lo, hi)
 }
